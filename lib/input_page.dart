77fb17bb28eaa2ce5e830bedb29f9d0669cbae47
saleh alghamdi <saleh@salehs-MacBook-Pro.local>
2019-11-04 18:52:20 +0300

    height card done with slider functionality

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -83,17 +83,25 @@ int height = 180;
                     ),
                   ],
                 ),
-                Slider(
-                  value: height.toDouble(),
-                  min: 120,
-                  max: 220,
-                  activeColor: Color(0xFFEB1555),
-                  inactiveColor: Color(0xFF8D8E98),
-                  onChanged: (double newValue){
-                    setState(() {
-                      height = newValue.round();
-                    });
-                  },
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                    thumbColor: Color(0xFFeb1555),
+                    overlayColor: Color(0x29EB1555),
+                  ),
+                  child: Slider(
+                    value: height.toDouble(),
+                    min: 120,
+                    max: 220,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue){
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  ),
                 ),
               ],
             ),
